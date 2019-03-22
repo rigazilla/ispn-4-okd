9ba677d7864f4a2b002aebfb2cfdff1e8e7c4958
Vittorio Rigamonti <rigazilla@tokelau.station>
2019-03-22 17:51:47 +0100

    initial

---
+++ src/main/java/org/infinispan/tutorial/okddnsping/Main.java
@@ -0,0 +1,23 @@
+package org.infinispan.tutorial.okddnsping;
+
+import java.io.IOException;
+
+import org.infinispan.Cache;
+import org.infinispan.configuration.global.GlobalConfigurationBuilder;
+import org.infinispan.manager.DefaultCacheManager;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		System.setProperty("java.net.preferIPv4Stack", "true");
+		//GlobalConfigurationBuilder gcb = GlobalConfigurationBuilder.defaultClusteredBuilder();
+		DefaultCacheManager manager = new DefaultCacheManager("cluster-dns-ping.xml");
+		manager.start();		
+		Cache<String, String> defaultCache = manager.getCache();
+		String s = defaultCache.get("first");
+		System.out.println("got: "+s);
+		defaultCache.put("first","first");
+		System.out.println("put");		
+	}
+
+}
