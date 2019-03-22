d18a0c125f30515359b7def18772bf2d608130cc
Vittorio Rigamonti <vrigamon@redhat.com>
2019-03-23 00:38:29 +0100

    debug

--- src/main/java/org/infinispan/tutorial/okddnsping/Main.java
+++ src/main/java/org/infinispan/tutorial/okddnsping/Main.java
@@ -2,6 +2,9 @@ package org.infinispan.tutorial.okddnsping;
 
 import java.io.IOException;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.config.Configurator;
 import org.infinispan.Cache;
 import org.infinispan.configuration.global.GlobalConfigurationBuilder;
 import org.infinispan.manager.DefaultCacheManager;
@@ -9,6 +12,7 @@ import org.infinispan.manager.DefaultCacheManager;
 public class Main {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
+		Configurator.setLevel(LogManager.getLogger("org.jgroups").getName(), Level.DEBUG);
 		System.setProperty("java.net.preferIPv4Stack", "true");
 		//GlobalConfigurationBuilder gcb = GlobalConfigurationBuilder.defaultClusteredBuilder();
 		DefaultCacheManager manager = new DefaultCacheManager("cluster-dns-ping.xml");
